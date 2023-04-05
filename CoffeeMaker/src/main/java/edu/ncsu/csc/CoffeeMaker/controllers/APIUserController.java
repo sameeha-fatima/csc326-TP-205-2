936bfb5fd5b4c7e1e4f383cb8efae771b1e3c1e0
cathy sun <cqsun@ncsu.edu>
2023-04-05 17:56:36 -0400

    [Fix] reverted changes to commit ba8c94f bc everything failed. #16

--- CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/controllers/APIUserController.java
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/controllers/APIUserController.java
@@ -10,11 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import edu.ncsu.csc.CoffeeMaker.models.Customer;
-import edu.ncsu.csc.CoffeeMaker.models.Staff;
 import edu.ncsu.csc.CoffeeMaker.models.User;
-import edu.ncsu.csc.CoffeeMaker.services.CustomerService;
-import edu.ncsu.csc.CoffeeMaker.services.StaffService;
+import edu.ncsu.csc.CoffeeMaker.services.UserService;
 
 /**
  * Class to handle Rest API calls dealing with the User class. To be used by the
@@ -27,130 +24,108 @@ import edu.ncsu.csc.CoffeeMaker.services.StaffService;
  *
  */
 @RestController
-@SuppressWarnings({ "rawtypes", "unchecked" })
+@SuppressWarnings ( { "rawtypes", "unchecked" } )
 public class APIUserController extends APIController {
 
-	/** Represents the service class used to access User database */
-	@Autowired
-	private StaffService sService;
+    /**
+     * UserService object, to be autowired in by Spring to allow for
+     * manipulating the User model
+     */
+    @Autowired
+    private UserService userService;
 
-	/** Represents the service class used to access User database */
-	@Autowired
-	private CustomerService cService;
+    /**
+     * REST API method to provide GET access to all users in the system
+     *
+     * @param id
+     *            represents the user ID / username used to find a user from the
+     *            database
+     * @return JSON representation of the found user or error message if none is
+     *         found
+     */
+    @GetMapping ( BASE_PATH + "/users/{id}" )
+    public ResponseEntity getUser ( @PathVariable ( "id" ) final String id ) {
+        final User user = userService.findByName( id );
+        return null == user
+                ? new ResponseEntity( errorResponse( "No User found for username " + id ), HttpStatus.NOT_FOUND )
+                : new ResponseEntity( user, HttpStatus.OK );
+    }
 
-	/**
-	 * REST API method to provide GET access to all users in the system
-	 *
-	 * @param id represents the user ID / username used to find a user from the
-	 *           database
-	 * @return JSON representation of the found user or error message if none is
-	 *         found
-	 */
-	@GetMapping(BASE_PATH + "/users/{id}")
-	public ResponseEntity getUser(@PathVariable("id") final String id) {
-		final User user = sService.findByName(id);
-		if (user == null) {
-			cService.findByName(id);
-		}
-		return null == user
-				? new ResponseEntity(errorResponse("No User found for username " + id), HttpStatus.NOT_FOUND)
-				: new ResponseEntity(user, HttpStatus.OK);
-	}
+    /**
+     * REST API method to provide POST access to the User model. This is used to
+     * create a new User by automatically converting the JSON RequestBody
+     * provided to a User object. Invalid JSON will fail.
+     *
+     * @param user
+     *            The valid User to be saved.
+     * @return ResponseEntity indicating success if the User could be saved to
+     *         the CoffeeMaker system, or an error if it could not be
+     */
+    @PostMapping ( BASE_PATH + "/users" )
+    public ResponseEntity createUser ( @RequestBody final User user ) {
+        if ( null != userService.findByName( user.getUsername() ) ) {
+            return new ResponseEntity( errorResponse( "User with the id " + user.getUsername() + " already exists" ),
+                    HttpStatus.CONFLICT );
+        }
+        final User newUser = null;
+        try {
+            userService.save( user );
+            return new ResponseEntity( newUser, HttpStatus.OK );
+        }
+        catch ( final Exception e ) {
+            return new ResponseEntity(
+                    errorResponse( "Could not create " + user.getUsername() + " because of " + e.getMessage() ),
+                    HttpStatus.BAD_REQUEST );
+        }
+    }
 
-	/**
-	 * REST API method to provide POST access to the User model. This is used to
-	 * create a new User by automatically converting the JSON RequestBody provided
-	 * to a User object. Invalid JSON will fail.
-	 *
-	 * @param user The valid User to be saved.
-	 * @return ResponseEntity indicating success if the User could be saved to the
-	 *         CoffeeMaker system, or an error if it could not be
-	 */
-	@PostMapping(BASE_PATH + "/users")
-	public ResponseEntity createUser(@RequestBody final User user) {
-		User found = sService.findByName(user.getUsername());
-		if (found == null) {
-			found = cService.findByName(user.getUsername());
-		}
-		if (null != found) {
-			return new ResponseEntity(errorResponse("User with the id " + user.getUsername() + " already exists"),
-					HttpStatus.CONFLICT);
-		}
-//		final User newUser = null;
-		try {
-			if (user instanceof Customer) {
-				final Customer newUser = (Customer) user;
-				cService.save(newUser);
-				return new ResponseEntity(newUser, HttpStatus.OK);
-			} else {
-				final Staff newUser = (Staff) user;
-				sService.save(newUser);
-				return new ResponseEntity(newUser, HttpStatus.OK);
-			}
-		} catch (final Exception e) {
-			return new ResponseEntity(
-					errorResponse("Could not create " + user.getUsername() + " because of " + e.getMessage()),
-					HttpStatus.BAD_REQUEST);
-		}
-	}
+    /**
+     * Method used to login to a new user in the system. Return error message if
+     * there are errors during the login process
+     *
+     * @param user
+     *            represents user to search for in the CoffeeMaker system
+     * @return User object with 200 status if successful login or an error
+     *         response (400) if not
+     */
+    public ResponseEntity login ( final User user ) {
+        final User foundUser = userService.findByName( user.getUsername() );
+        if ( null == foundUser ) {
+            new ResponseEntity( errorResponse( "No User found for username " + user.getUsername() ),
+                    HttpStatus.NOT_FOUND );
+        }
+        final String inputPasswrd = user.getPassword();
+        if ( inputPasswrd.equals( foundUser.getPassword() ) ) {
+            return new ResponseEntity( user, HttpStatus.OK );
+        }
+        return new ResponseEntity( errorResponse( "Password is incorrect" ), HttpStatus.BAD_REQUEST );
 
-	/**
-	 * Method used to login to a new user in the system. Return error message if
-	 * there are errors during the login process
-	 *
-	 * @param user represents user to search for in the CoffeeMaker system
-	 * @return User object with 200 status if successful login or an error response
-	 *         (400) if not
-	 */
-	public ResponseEntity login(final User user) {
-		User found = sService.findByName(user.getUsername());
-		if (found == null) {
-			found = cService.findByName(user.getUsername());
-		}
-		if (null == found) {
-			new ResponseEntity(errorResponse("No User found for username " + user.getUsername()), HttpStatus.NOT_FOUND);
-		}
-		final String inputPasswrd = user.getPassword();
-		if (inputPasswrd.equals(found.getPassword())) {
-			return new ResponseEntity(user, HttpStatus.OK);
-		}
-		return new ResponseEntity(errorResponse("Password is incorrect"), HttpStatus.BAD_REQUEST);
+    }
 
-	}
+    /**
+     * REST API method to allow deleting a User from the CoffeeMaker's
+     * Inventory, by making a DELETE request to the API endpoint and indicating
+     * the User to delete (as a path variable)
+     *
+     * @param id
+     *            The username of the User to delete
+     * @return Success if the User could be deleted; an error if the User does
+     *         not exist
+     */
+    @DeleteMapping ( BASE_PATH + "/users/{id}" )
+    public ResponseEntity deleteUser ( @PathVariable final String id ) {
+        final User user = userService.findByName( id );
+        try {
+            if ( null == user ) {
+                return new ResponseEntity( errorResponse( "No user found for id " + id ), HttpStatus.NOT_FOUND );
+            }
+            userService.delete( user );
+            return new ResponseEntity( id, HttpStatus.OK );
+        }
+        catch ( final Exception e ) {
+            return new ResponseEntity( errorResponse( "Could not delete " + id + " because of " + e.getMessage() ),
+                    HttpStatus.BAD_REQUEST );
+        }
 
-	/**
-	 * REST API method to allow deleting a User from the CoffeeMaker's Inventory, by
-	 * making a DELETE request to the API endpoint and indicating the User to delete
-	 * (as a path variable)
-	 *
-	 * @param id The username of the User to delete
-	 * @return Success if the User could be deleted; an error if the User does not
-	 *         exist
-	 */
-	@DeleteMapping(BASE_PATH + "/users/{id}")
-	public ResponseEntity deleteUser(@PathVariable final String id) {
-		boolean isStaff = false;
-		User found = sService.findByName(id);
-
-		if (found == null) {
-			found = cService.findByName(id);
-		} else {
-			isStaff = true;
-		}
-		try {
-			if (null == found) {
-				return new ResponseEntity(errorResponse("No user found for id " + id), HttpStatus.NOT_FOUND);
-			}
-			if (isStaff) {
-				sService.delete((Staff) found);
-			} else {
-				cService.delete((Customer) found);
-			}
-			return new ResponseEntity(id, HttpStatus.OK);
-		} catch (final Exception e) {
-			return new ResponseEntity(errorResponse("Could not delete " + id + " because of " + e.getMessage()),
-					HttpStatus.BAD_REQUEST);
-		}
-
-	}
+    }
 }
--- CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/api/APIUserTest.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/api/APIUserTest.java
@@ -1,11 +1,5 @@
 package edu.ncsu.csc.CoffeeMaker.api;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
-
 import javax.transaction.Transactional;
 
 import org.junit.Test;
@@ -14,16 +8,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import edu.ncsu.csc.CoffeeMaker.common.TestUtils;
-import edu.ncsu.csc.CoffeeMaker.models.Customer;
-import edu.ncsu.csc.CoffeeMaker.services.CustomerService;
-import edu.ncsu.csc.CoffeeMaker.services.StaffService;
+import edu.ncsu.csc.CoffeeMaker.services.UserService;
 
 /**
  * Tests User database interactions, including login functionality
@@ -31,78 +21,48 @@ import edu.ncsu.csc.CoffeeMaker.services.StaffService;
  * @author Ellie Murphy
  *
  */
-@ExtendWith(SpringExtension.class)
+@ExtendWith ( SpringExtension.class )
 @SpringBootTest
 @AutoConfigureMockMvc
 class APIUserTest {
 
-	/**
-	 * MockMvc uses Spring's testing framework to handle requests to the REST API
-	 */
-	private MockMvc mvc;
+    /**
+     * MockMvc uses Spring's testing framework to handle requests to the REST
+     * API
+     */
+    private MockMvc               mvc;
 
-	/** context for mvc object */
-	@Autowired
-	private WebApplicationContext context;
+    /** context for mvc object */
+    @Autowired
+    private WebApplicationContext context;
 
-	/** UserService object for testing */
-	@Autowired
-	private StaffService sService;
+    /** UserService object for testing */
+    @Autowired
+    private UserService           service;
 
-	/** UserService object for testing */
-	@Autowired
-	private CustomerService cService;
+    /**
+     * Sets up the tests.
+     */
+    @BeforeEach
+    public void setup () {
+        mvc = MockMvcBuilders.webAppContextSetup( context ).build();
 
-	/**
-	 * Sets up the tests.
-	 */
-	@BeforeEach
-	public void setup() {
-		mvc = MockMvcBuilders.webAppContextSetup(context).build();
+        service.deleteAll();
+    }
 
-		sService.deleteAll();
-		cService.deleteAll();
-	}
-
-	/**
-	 * Tests login API call
-	 *
-	 * @throws Exception if exception thrown during testing
-	 */
-	@Test
-	@Transactional
-	public void testCreateUser() throws Exception {
-		sService.deleteAll();
-		cService.deleteAll();
-
-		assertEquals(0, sService.findAll().size());
-		assertEquals(0, cService.findAll().size());
-		final Customer customer = new Customer("customer1", "thisisagoodpassword");
-		mvc.perform(
-				post("/api/v1/users").contentType(MediaType.APPLICATION_JSON).content(TestUtils.asJsonString(customer)))
-				.andExpect(status().isOk());
-		assertEquals(1, cService.findAll().size());
-		final List<Customer> contents = cService.findAll();
-		assertEquals(customer, contents.get(0));
-	}
-
-	/**
-	 * Tests login API call
-	 *
-	 * @throws Exception if exception thrown during testing
-	 */
-	@Test
-	@Transactional
-	public void testLogin() throws Exception {
-		sService.deleteAll();
-		cService.deleteAll();
-
-		final Customer customer = new Customer("customer1", "thisisagoodpassword");
-		mvc.perform(
-				post("/api/v1/users").contentType(MediaType.APPLICATION_JSON).content(TestUtils.asJsonString(customer)))
-				.andExpect(status().isOk());
-		assertEquals(1, cService.findAll().size());
-
-	}
+    /**
+     * Tests login API call
+     *
+     * @throws Exception
+     *             if exception thrown during testing
+     */
+    @Test
+    @Transactional
+    public void testLogin () throws Exception {
+        // final String user =
+        // mvc.perform(get("/api/v1/users")).andDo(print()).andExpect(status().isOk()).andReturn()
+        // .getResponse().getContentAsString();
+        // TODO
+    }
 
 }
