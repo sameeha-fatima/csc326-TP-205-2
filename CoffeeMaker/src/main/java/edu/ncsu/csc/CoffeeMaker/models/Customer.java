debc30358760938b3133917c3d66485fd065e561
evmurphy <evmurphy@ncsu.edu>
2023-04-05 13:31:06 -0400

    [Tests] Added tests for user creation. Refers to Issue #16

--- CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/Customer.java
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/Customer.java
@@ -4,15 +4,14 @@ import java.io.Serializable;
 
 public class Customer extends User {
 
-    public Customer ( final String username, final String password ) {
-        super( username, password );
-        // TODO Auto-generated constructor stub
-    }
+	public Customer(final String username, final String password) {
+		super(username, password);
+	}
 
-    @Override
-    public Serializable getId () {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public Serializable getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/unit/UserTest.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/unit/UserTest.java
@@ -1,6 +1,7 @@
 package edu.ncsu.csc.CoffeeMaker.unit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,79 +24,112 @@ import edu.ncsu.csc.CoffeeMaker.services.UserService;
  * @author Cathy Sun
  *
  */
-@ExtendWith ( SpringExtension.class )
+@ExtendWith(SpringExtension.class)
 @EnableAutoConfiguration
-@SpringBootTest ( classes = TestConfig.class )
+@SpringBootTest(classes = TestConfig.class)
 public class UserTest {
 
-    /** Represents the service class used to access User database */
-    @Autowired
-    private UserService service;
+	/** Represents the service class used to access User database */
+	@Autowired
+	private UserService service;
 
-    // private static final String USER1 = "user1";
-    //
-    // private static final String USER2 = "user2";
-    //
-    // private static final String USER3 = "user3";
-    //
-    // private static final String PW = "abc123";
+	// private static final String USER1 = "user1";
+	//
+	// private static final String USER2 = "user2";
+	//
+	// private static final String USER3 = "user3";
+	//
+	// private static final String PW = "abc123";
 
-    /**
-     * Set up
-     */
-    @BeforeEach
-    public void setup () {
-        service.deleteAll();
-    }
+	/**
+	 * Set up
+	 */
+	@BeforeEach
+	public void setup() {
+		service.deleteAll();
+	}
 
-    /**
-     * Tests valid user objects of both customer and staff
-     */
-    @Test
-    @Transactional
-    public void testUserValid () {
+	/**
+	 * Tests valid user objects of both customer and staff
+	 */
+	@Test
+	@Transactional
+	public void testUserValid() {
 
-        // testing creating a customer object
-        assertEquals( 0, service.count() );
-        final User c1 = new Customer( "customer1", "1111" );
-        // service.save( c1 );
-        assertEquals( c1.getUsername(), "customer1" );
-        assertEquals( c1.getPassword(), "1111" );
-        // assertEquals( 1, service.count() );
+		// testing creating a customer object
+		assertEquals(0, service.count());
+		final User c1 = new Customer("customer1", "iamapassword");
+		// service.save( c1 );
+		assertEquals(c1.getUsername(), "customer1");
+		assertEquals(c1.getPassword(), "iamapassword");
+		// assertEquals( 1, service.count() );
 
-        final User c2 = new Customer( "customer2", "2222" );
-        // service.save( c1 );
-        assertEquals( c2.getUsername(), "customer2" );
-        assertEquals( c2.getPassword(), "2222" );
+		final User c2 = new Customer("customer2", "iamapassword");
+		// service.save( c1 );
+		assertEquals(c2.getUsername(), "customer2");
+		assertEquals(c2.getPassword(), "iamapassword");
 
-        // testing creating a staff object
-        final User s1 = new Staff( "staff1", "1111" );
-        // service.save( c1 );
-        assertEquals( s1.getUsername(), "staff1" );
-        assertEquals( s1.getPassword(), "1111" );
-        // assertEquals( 1, service.count() );
+		// testing creating a staff object
+		final User s1 = new Staff("staff1", "iamapassword");
+		// service.save( c1 );
+		assertEquals(s1.getUsername(), "staff1");
+		assertEquals(s1.getPassword(), "iamapassword");
+		// assertEquals( 1, service.count() );
 
-        final User s2 = new Staff( "staff2", "2222" );
-        // service.save( c1 );
-        assertEquals( s2.getUsername(), "staff2" );
-        assertEquals( s2.getPassword(), "2222" );
+		final User s2 = new Staff("staff2", "iamapassword");
+		// service.save( c1 );
+		assertEquals(s2.getUsername(), "staff2");
+		assertEquals(s2.getPassword(), "iamapassword");
 
-    }
+		final User s3 = new Staff("staff2", "iamapassword!");
+		// service.save( c1 );
+		assertEquals(s3.getUsername(), "staff2");
+		assertEquals(s3.getPassword(), "iamapassword!");
 
-    /**
-     * Tests valid user objects of both customer and staff
-     */
-    @Test
-    @Transactional
-    public void testUserInValid () {
-        // TODO: Add length checks for password and username
+	}
 
-        // invalid customer username length
-        final User c1 = new Customer( "customer1", "1" );
-        // assertEquals( c1.getPassowrd(), "1" );
-        // final Exception e1 = assertThrows( IllegalArgumentException.class, ()
-        // -> c1.setPassword( "1" ) );
-        // assertEquals( "Invalid Ingredient amount.", e1.getMessage() );
-    }
+	/**
+	 * Tests valid user objects of both customer and staff
+	 */
+	@Test
+	@Transactional
+	public void testUserInValid() {
+		// invalid customer password length
+		try {
+			new Customer("customer1", "1");
+			fail("Should have thrown exception");
+		} catch (final IllegalArgumentException e) {
+			assertEquals("Incorrect IAE thrown", "Invalid password length.", e.getMessage());
+		}
+		try {
+			new Customer("customer1", "iamapasswordandiamgreat");
+			fail("Should have thrown exception");
+		} catch (final IllegalArgumentException e) {
+			assertEquals("Incorrect IAE thrown", "Invalid password length.", e.getMessage());
+		}
+		try {
+			new Customer("customer1", "iamapassword:");
+			fail("Should have thrown exception");
+		} catch (final IllegalArgumentException e) {
+			assertEquals("Incorrect IAE thrown", "Invalid characters in password.", e.getMessage());
+		}
+		// invalid customer username length
+		try {
+			new Customer("user", "iamapassword");
+			fail("Should have thrown exception");
+		} catch (final IllegalArgumentException e) {
+			assertEquals("Incorrect IAE thrown", "Invalid username length.", e.getMessage());
+		}
+		try {
+			new Customer("customer!", "iamapassword");
+			fail("Should have thrown exception");
+		} catch (final IllegalArgumentException e) {
+			assertEquals("Incorrect IAE thrown", "Invalid characters in username.", e.getMessage());
+		}
+		// assertEquals( c1.getPassowrd(), "1" );
+		// final Exception e1 = assertThrows( IllegalArgumentException.class, ()
+		// -> c1.setPassword( "1" ) );
+		// assertEquals( "Invalid Ingredient amount.", e1.getMessage() );
+	}
 
 }
