fc0858bb4c816a161b2e6591c29476459b6bbd38
evmurphy <evmurphy@ncsu.edu>
2023-04-05 18:10:39 -0400

    [Implementation] Added User enumerations. Refers to Issue #14

---
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/UserEnum.java
@@ -0,0 +1,12 @@
+package edu.ncsu.csc.CoffeeMaker.models;
+
+/**
+ * Enumeration types for possible user accounts in CoffeeMaker system
+ *
+ * @author Ellie Murphy
+ *
+ */
+public enum UserEnum {
+	/** User can either be a staff or customer */
+	STAFF, CUSTOMER;
+}
