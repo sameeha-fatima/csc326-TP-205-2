5e6d0696d8ece21a66fc3919c541b2dab834838a
DevTime217 <headshotgamingmaster@gmail.com>
2023-04-05 18:27:49 -0400

    [TP] Implementation of Login HTML

--- CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/controllers/MappingController.java
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/controllers/MappingController.java
@@ -13,6 +13,17 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class MappingController {
+    /**
+     * On a GET request to /login, the IndexController will return
+     * /src/main/resources/templates/login.html.
+     *
+     * @param model underlying UI model
+     * @return contents of the page
+     */
+    @GetMapping({"/login", "/"})
+    public String login(final Model model) {
+        return "login";
+    }
 
 	/**
 	 * On a GET request to /index, the IndexController will return
@@ -21,7 +32,7 @@ public class MappingController {
 	 * @param model underlying UI model
 	 * @return contents of the page
 	 */
-	@GetMapping({ "/index", "/" })
+	@GetMapping({ "/index", "/index.html" })
 	public String index(final Model model) {
 		return "index";
 	}
