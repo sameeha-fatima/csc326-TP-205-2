7526f7e638833ac8e2728366296e40f5065c588e
Casey <cbdocke2@ncsu.edu>
2023-04-11 17:32:00 -0400

    [Implementation] Started the Order class for UC-10

---
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/Order.java
@@ -0,0 +1,89 @@
+package edu.ncsu.csc.CoffeeMaker.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.Length;
+
+/**
+ *
+ * @author cbdocke2
+ *
+ */
+public class Order extends DomainObject {
+
+    /** Order id */
+    @Id
+    @GeneratedValue
+    private Long    id;
+
+    /**
+     * Represents the username identifier for a User object, must be between 6
+     * and 20 characters
+     */
+    @Length ( min = 6, max = 20 )
+    private String  customerUsername;
+
+    private Recipe  recipe;
+
+    private boolean isFulfilled;
+
+    /**
+     * Creates a default user for the CoffeeMaker system. Used by Hibernate
+     */
+    public Order () {
+        super();
+    }
+
+    /**
+     * All-argument constructor for User
+     *
+     * @param name
+     *            represents username of User object
+     * @param password
+     *            represents password of User object
+     * @param userType
+     *            Enumeration representing type of user account
+     */
+    public Order ( final String customerUsername, final Recipe recipe, final boolean isFulfilled ) {
+        this();
+        // setPassword(password);
+        // setUserType(userType);
+        // setName(name);
+    }
+
+    @Override
+    public Long getId () {
+        return id;
+    }
+
+    @SuppressWarnings ( "unused" )
+    private void setId ( final Long id ) {
+        this.id = id;
+    }
+
+    public String getCustomerUsername () {
+        return customerUsername;
+    }
+
+    public void setCustomerUsername ( final String customerUsername ) {
+        this.customerUsername = customerUsername;
+    }
+
+    public Recipe getRecipe () {
+        return recipe;
+    }
+
+    public void setRecipe ( final Recipe recipe ) {
+        this.recipe = recipe;
+    }
+
+    public boolean isFulfilled () {
+        return isFulfilled;
+    }
+
+    public void setFulfilled ( final boolean isFulfilled ) {
+        this.isFulfilled = isFulfilled;
+    }
+
+}
