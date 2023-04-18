003da35d4138a2ca50db6bbc4e275a7733310f70
Casey <cbdocke2@ncsu.edu>
2023-04-18 16:47:31 -0400

    [APITesting] Added testing for the GetOrder API call

--- CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/api/APICustomerOrderTest.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/api/APICustomerOrderTest.java
@@ -1,8 +1,11 @@
 package edu.ncsu.csc.CoffeeMaker.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
@@ -103,7 +106,7 @@ public class APICustomerOrderTest {
     }
 
     /**
-     * Tests adding Orders
+     * Tests editing Orders
      *
      * @throws Exception
      *             if something is wrong
@@ -131,6 +134,56 @@ public class APICustomerOrderTest {
                 .content( TestUtils.asJsonString( order3 ) ) ).andExpect( status().isOk() );
 
         assertEquals( service.findByName( "Order3" ).isFulfilled(), true );
+
+    }
+
+    /**
+     * Tests getting Orders
+     *
+     * @throws Exception
+     *             if something is wrong
+     */
+    @Test
+    @Transactional
+    public void testGetOrder () throws Exception {
+
+        assertEquals( 0, service.findAll().size(), "There should be no Orders in the CoffeeMaker" );
+        final List<Recipe> recipes = new ArrayList<Recipe>();
+        final List<Ingredient> ingredients1 = new ArrayList<Ingredient>();
+        final Ingredient ingredient1 = new Ingredient( "Ingredient1", 12 );
+        ingredients1.add( ingredient1 );
+        final Recipe recipe1 = new Recipe( ingredients1, 12 );
+        recipes.add( recipe1 );
+        final CustomerOrder order1 = new CustomerOrder( "Order1", "Customer", recipes );
+
+        mvc.perform( post( "/api/v1/orders" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( order1 ) ) ).andExpect( status().isOk() );
+        assertEquals( 1, service.findAll().size(), "There should be one Order in CoffeeMaker" );
+
+        assertEquals( order1, service.findByName( "Order1" ) );
+
+        final List<Recipe> recipes2 = new ArrayList<Recipe>();
+        final List<Ingredient> ingredients2 = new ArrayList<Ingredient>();
+        final Ingredient ingredient2 = new Ingredient( "Ingredient2", 12 );
+        ingredients1.add( ingredient2 );
+        final Recipe recipe2 = new Recipe( ingredients2, 12 );
+        recipes.add( recipe2 );
+        final CustomerOrder order2 = new CustomerOrder( "Order2", "Customer", recipes2 );
+
+        mvc.perform( post( "/api/v1/orders" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( order2 ) ) ).andExpect( status().isOk() );
+        assertEquals( 2, service.findAll().size(), "There should be two Orders in CoffeeMaker" );
+
+        final String orderstring1 = mvc.perform( get( String.format( "/api/v1/orders/%s", "Order1" ) ) )
+                .andDo( print() ).andExpect( status().isOk() ).andReturn().getResponse().getContentAsString();
+        assertTrue( orderstring1.contains( "Order1" ) );
+        final String orderstring2 = mvc.perform( get( String.format( "/api/v1/orders/%s", "Order2" ) ) )
+                .andDo( print() ).andExpect( status().isOk() ).andReturn().getResponse().getContentAsString();
+        assertTrue( orderstring2.contains( "Order2" ) );
+
+        // no order found with this name
+        mvc.perform( get( String.format( "/api/v1/orders/%s", "Order12No" ) ) )
+                .andExpect( status().is4xxClientError() );
 
     }
 
